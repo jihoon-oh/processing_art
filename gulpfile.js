347773a43fd0cf0877a37515aad9335c6bad3023
Jihoon Oh <jihoonoh123@gmail.com>
2016-11-21 12:00:18 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+
+gulp.task('default', ['browser-sync'], function () {
+});
+
+// gulp.task('watch', ['browser-sync'], function () {
+//     gulp.watch("*.html").on('change', browserSync.reload);
+// });
+
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null, {
+		proxy: "http://localhost:3000",
+        files: ["css/*.css"],
+        browser: "google chrome",
+        port: 3000,
+	});
+});
+gulp.task('nodemon', function (cb) {
+	
+	var started = false;
+	
+	return nodemon({
+		script: 'server.js'
+	}).on('start', function () {
+		// to avoid nodemon being started multiple times
+		// thanks @matthisk
+		if (!started) {
+			cb();
+			started = true; 
+		} 
+	});
+});
